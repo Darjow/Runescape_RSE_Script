d603574d907168e4be53696958b2cf216640dee2
DarioBronders <dario.bronders@student.hogent.be>
2022-10-22 23:55:58 +0200

    init

---
+++ src/script/Script.java
@@ -0,0 +1,14 @@
+package script;
+
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+
+@ScriptManifest(author = "Shock#5170", name = "sRedSpiderEggs", version = 1.0, description = "Private script for MrWalkway, no reselling is allowed", category = Category.MONEYMAKING)
+public class Script extends AbstractScript {
+
+    @Override
+    public int onLoop() {
+        return 0;
+    }
+}
